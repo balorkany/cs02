f9463f0ae01cc1582f098e9559261205be99b65c
Bálint Varga <balorkany@gmail.com>
2022-04-06 18:30:35 +0200

    Hello World magyarítás.

--- gitProba/Program.cs
+++ gitProba/Program.cs
@@ -6,7 +6,7 @@ namespace gitProba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Szerbusz Világ!");
         }
     }
 }
