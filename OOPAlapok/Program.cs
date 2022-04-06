83292f213ac107d4c9d7b9181be981b530a8fafd
Bálint Varga <balorkany@gmail.com>
2022-04-06 22:06:19 +0200

    OOP Alapok

---
+++ OOPAlapok/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPAlapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szemely> szemelyek = new List<Szemely>()
+            {
+                new Szemely("Pipoly Ivett"),
+                new Szemely("Nagy Béla"),
+                new Szemely("Kiss Géza")
+            };
+
+
+            Szemely sz = new Szemely();
+
+            Console.WriteLine($"{sz.Csaladnev} {sz.Vezeteknev}");
+            Console.WriteLine(sz.TeljesNev);
+
+            Console.WriteLine(sz.Csaladnev);
+            
+
+            Szemely sz2 = null;
+
+            foreach (Szemely szemely in szemelyek)
+            {
+                Console.WriteLine(szemely);
+            }
+
+            Console.WriteLine(Szemely.Nagybetusse("ertertre"));
+            
+
+        }
+
+
+    }
+
+
+
+}
---
+++ OOPAlapok/Szemely.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace OOPAlapok
+{
+    class Szemely
+    {
+        static public int staticSorszam = 0;
+
+        private int _sorszam;
+
+        public int Sorszam
+        {
+            get
+            {
+                return this._sorszam;
+            }
+        }
+
+
+        private string _csaladnev;
+
+        public string Csaladnev {
+            get 
+            {
+                return this._csaladnev;
+            }
+            set 
+            {
+                this._csaladnev = Szemely.Nagybetusse(value);
+            } 
+        }
+
+
+        //private string _vezeteknev;
+
+        //public string Vezeteknev
+        //{
+        //    get
+        //    {
+        //        return this._vezeteknev;
+        //    }
+
+        //    set
+        //    {
+        //        this._vezeteknev = value;
+        //    }
+        //}
+
+        public string Vezeteknev;
+
+        public string TeljesNev
+        {
+            get
+            {
+                return $"{this.Csaladnev} {this.Vezeteknev}";
+            }
+        }
+
+
+        public Szemely(string csaladnev, string vezeteknev)
+        {
+            staticSorszam++;
+            this._sorszam = staticSorszam;
+            this.Csaladnev = csaladnev;
+            this.Vezeteknev = vezeteknev;
+        }
+
+        public Szemely(string teljesnev)
+        {
+            staticSorszam++;
+            this._sorszam = staticSorszam;
+
+            string[] nevek = teljesnev.Split(" ");
+
+            this.Csaladnev = nevek[0];
+            this.Vezeteknev = nevek[1];
+        }
+
+        public Szemely()
+        {
+            staticSorszam++;
+            this._sorszam = staticSorszam;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Sorszam}. {this.TeljesNev}";
+        }
+
+        static public string Nagybetusse(string s) => s.ToUpper()[0] + s.Substring(1);
+
+    }
+}
