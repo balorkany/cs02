d8be5db4fcf8a1f9719cd2ef7e5f32bca7fff1b1
Bálint Varga <balorkany@gmail.com>
2022-04-06 18:35:39 +0200

    Új magyarítás

--- gitProba/Program.cs
+++ gitProba/Program.cs
@@ -6,7 +6,7 @@ namespace gitProba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Szerbusz Világ!");
+            Console.WriteLine("Helló Világ!");
         }
     }
 }
