d93d80cad15c7dbd366395b11d0bd3ff5cb8ba6e
Bálint Varga <balorkany@gmail.com>
2022-04-06 18:14:41 +0200

    Add project files.

---
+++ gitProba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitProba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
